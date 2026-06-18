0cdcccd856c7e3cae390c9071c5615991e7cff54
Leonel Rodriguez <leorodriguezjesus@gmail.com>
2026-06-18 14:31:39 -0300

    .

--- UI/frmCambiarIdioma.Designer.cs
+++ UI/frmCambiarIdioma.Designer.cs
@@ -34,30 +34,30 @@
             btnVolver = new Button();
             lblMensaje = new Label();
             SuspendLayout();
-            //
+            // 
             // lblIdioma
-            //
+            // 
             lblIdioma.AutoSize = true;
             lblIdioma.Font = new Font("Segoe UI", 10F);
             lblIdioma.Location = new Point(30, 35);
             lblIdioma.Name = "lblIdioma";
-            lblIdioma.Size = new Size(58, 19);
+            lblIdioma.Size = new Size(54, 19);
             lblIdioma.TabIndex = 0;
             lblIdioma.Text = "Idioma:";
-            //
+            // 
             // cboIdiomas
-            //
+            // 
             cboIdiomas.DropDownStyle = ComboBoxStyle.DropDownList;
             cboIdiomas.Font = new Font("Segoe UI", 10F);
             cboIdiomas.FormattingEnabled = true;
             cboIdiomas.Location = new Point(110, 32);
             cboIdiomas.Name = "cboIdiomas";
-            cboIdiomas.Size = new Size(240, 27);
+            cboIdiomas.Size = new Size(240, 25);
             cboIdiomas.TabIndex = 1;
-            //
+            // 
             // btnGuardar
-            //
-            btnGuardar.BackColor = Color.LightSkyBlue;
+            // 
+            btnGuardar.BackColor = SystemColors.ButtonHighlight;
             btnGuardar.Font = new Font("Segoe UI", 10F);
             btnGuardar.Location = new Point(110, 110);
             btnGuardar.Name = "btnGuardar";
@@ -66,9 +66,9 @@
             btnGuardar.Text = "Guardar";
             btnGuardar.UseVisualStyleBackColor = false;
             btnGuardar.Click += btnGuardar_Click;
-            //
+            // 
             // btnVolver
-            //
+            // 
             btnVolver.Font = new Font("Segoe UI", 10F);
             btnVolver.Location = new Point(240, 110);
             btnVolver.Name = "btnVolver";
@@ -77,9 +77,9 @@
             btnVolver.Text = "Volver";
             btnVolver.UseVisualStyleBackColor = true;
             btnVolver.Click += btnVolver_Click;
-            //
+            // 
             // lblMensaje
-            //
+            // 
             lblMensaje.AutoSize = true;
             lblMensaje.Font = new Font("Segoe UI", 9F);
             lblMensaje.ForeColor = Color.DarkRed;
@@ -87,25 +87,27 @@
             lblMensaje.Name = "lblMensaje";
             lblMensaje.Size = new Size(0, 15);
             lblMensaje.TabIndex = 4;
-            //
+            // 
             // frmCambiarIdioma
-            //
+            // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.PowderBlue;
             ClientSize = new Size(400, 180);
             Controls.Add(lblMensaje);
             Controls.Add(btnVolver);
             Controls.Add(btnGuardar);
             Controls.Add(cboIdiomas);
             Controls.Add(lblIdioma);
+            ForeColor = SystemColors.ControlText;
             FormBorderStyle = FormBorderStyle.FixedSingle;
             MaximizeBox = false;
             MinimizeBox = false;
             Name = "frmCambiarIdioma";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Cambiar Idioma";
-            Load += frmCambiarIdioma_Load;
             FormClosed += frmCambiarIdioma_FormClosed;
+            Load += frmCambiarIdioma_Load;
             ResumeLayout(false);
             PerformLayout();
         }
